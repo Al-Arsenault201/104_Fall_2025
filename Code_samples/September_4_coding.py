b80a17c85a26ec6817395cf805d34104b58d0168
Al-Arsenault201 <69997839+Al-Arsenault201@users.noreply.github.com>
2025-09-09 15:31:56 -0400

    Create September_4_coding.py

---
+++ Code_samples/September_4_coding.py
@@ -0,0 +1,15 @@
+# this code will print the integers 1 through 10, inclusive
+# one integer per line
+for index in range(1,11,1):
+    print(index)
+
+# This code will print the string "Hello, world" ten times
+# once per line
+for index in range(1,11,1):
+    print("Hello, world")
+
+"""
+This is a multi-line comment. Everything between these to sets of 
+three, double-quotes, is treated as a single comment. 
+
+"""

f59005323ff44964ebf0cd7cd5f4e53e17e2a07c
Al-Arsenault201 <69997839+Al-Arsenault201@users.noreply.github.com>
2025-09-11 12:26:47 -0400

    Add files via upload

---
+++ Code_samples/September_9_coding.py
@@ -0,0 +1,9 @@
+# I want the user to enter their first mame
+
+fname = input("Please enter your first name: ")
+print(fname)
+
+for  i in range(0,5,1):
+    print(i)
+    input("Hit enter to continue:")
+          

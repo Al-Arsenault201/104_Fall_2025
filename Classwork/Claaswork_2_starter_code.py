6e00d8d26d925d61480f95f5d1d6b3d3c600cc78
Al-Arsenault201 <69997839+Al-Arsenault201@users.noreply.github.com>
2025-09-11 16:53:35 -0400

    Add files via upload

---
+++ Classwork/Claaswork_2_starter_code.py
@@ -0,0 +1,25 @@
+# Name : Alice Smith ( your name here !)
+
+# Part 1: Replace ?? with a list of things to print , such as
+# favourite foods , summer activities , pet names , etc.
+ for item in ??:
+  print ( item )
+
+# Part 2: Prompt the user for 4 words : a noun , a verb , an adjective ,
+# and a place . Then , display the words as a sentence .
+noun = ???
+verb = ???
+adj = ???
+place = ???
+print (" Bring your " + ???)
+
+# Part 3: As of July 2024 , the Voyager 1 spacecraft is about
+# 24 ,447 ,732 ,226 kilometers from Earth . Radio transmissions are about
+# the speed of light : 300 ,000 kilometers per second . Write code which
+# calculates the estimated transmission time for one -way communication
+# with Voyager 1 in hours . Estimated answer : 22.6 hours .
+distance_over_speed = 24447732226.0/300000.0
+
+# Convert from seconds to hours
+
+# Print the result and include a description .
